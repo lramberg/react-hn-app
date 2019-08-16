f8a11275d578b4bac14afceb292f4ec03b2c6b68
Lyndsay Ramberg <lyndsayramberg@Lyndsays-MacBook-Pro.local>
2019-08-16 16:55:13 -0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
+// components
+import HomePage from './pages/HomePage/HomePage';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Switch>
+                <Route  exact path='/' render={() => 
+                    <HomePage />
+                } />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const HomePage = () => {
+  return (
+    <div className="App">
+        Home Page
+    </div>
+  );
+};
+
+export default HomePage;
